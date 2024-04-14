0871dd24945aeb908da310018ef71c3da3376c01
Daniel Philipov <dp33@illinois.edu>
2024-04-14 00:21:15 -0500

    remove nix-shell prompt bc annoying

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -181,7 +181,7 @@ if set -q right_segment_separator
     set -g RETVAL $status
     prompt_status
     prompt_os
-    prompt_virtual_env
+    # prompt_virtual_env
     prompt_user
     prompt_dir
     if [ (cwd_in_scm_blacklist | wc -c) -eq 0 ]
