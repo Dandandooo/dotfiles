9162bde50e63668acfcd2f07704f53c47d7e0eb8
Daniel Philipov <dp33@illinois.edu>
2024-09-08 15:14:12 -0500

    plugins2

--- home-manager/modules/linux/hyprland.nix
+++ home-manager/modules/linux/hyprland.nix
@@ -1,16 +1,28 @@
-{ inputs, config, lib, pkgs, ... }: let
+{ inputs, pkgs, ... }: let
 
-    # hypr-plugins = inputs.hyprland-plugins-packages.${pkgs.stdenv.hostPlatform.system};
-    hypr-plugins = pkgs.hyprPlugins;
+    hypr-plugins = inputs.hyprland-plugins.packages.${pkgs.system};
 
 in {
-
     wayland.windowManager.hyprland = {
+        enable = true;
+        systemd.enable = false;
         plugins = [
-            hypr-plugins.hy3
-            hypr-plugins.hyprtrails
-            hypr-plugins."border++"
+            # hypr-plugins.hy3
+            # hypr-plugins.hyprtrails
+            hypr-plugins.borders-plus-plus
         ];
+
+        settings = {
+            "plugin:borders-plus-plus" = {
+                add_borders = 1;
+                "col.border1" = "0x000000";
+                "col.border2" = "0x000000";
+                border-size_1 = 2;
+                border-size_2 = 2;
+                natural_rounding = "yes";
+            };
+
+        };
     };
 
 }
